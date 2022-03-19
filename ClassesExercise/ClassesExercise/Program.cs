fc889a8fcbd7322c86c3cb81dfb63cf8671824ec
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-19 16:29:34 -0500

    inital commit

---
+++ ClassesExercise/ClassesExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Nissan";
+            myCar.Model = "Versa";
+            myCar.Year = "2017";
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model); 
+            Console.WriteLine(myCar.Year);
+            
+        }
+    }
+}
