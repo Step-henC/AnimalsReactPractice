c313589b52c3eaf3326c0d422085a31da083c528
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-19 22:07:08 -0500

    initial commit

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public double MoneyDep { get; set; }
+        
+        public void Deposit(double baLance)
+        {
+           balance += baLance;
+        }
+        public double GetBalance()
+        {
+            Console.WriteLine("Do you want the balance?");
+            string answer = Console.ReadLine();
+            if (answer == null)
+            { return 1; }
+
+            Console.WriteLine(balance);
+            return 0;
+        }
+    }
+}
