f57e0d26c8e40201621bed36ea9b8e35b076d8e5
Stephen Cunningham <stevecunningham@outlook.com>
2022-06-20 21:48:35 -0500

    initial commit

---
+++ ReactAnimals/wwwroot/js/animals.js
@@ -0,0 +1,14 @@
+﻿export const animals = {
+    dolphin: {
+        image: 'C:\Users\steve\repos\ReactAnimals\Pages\Shared\dolphin.jpg',
+        facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+    },
+    lobster: {
+        image: 'C:\Users\steve\repos\ReactAnimals\Pages\Shared\lobster.jpg',
+        facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+    },
+    starfish: {
+        image: 'C:\Users\steve\repos\ReactAnimals\Pages\Shared\starfish.jpg',
+        facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+    }
+};
---
+++ ReactAnimals/wwwroot/js/app.js
@@ -0,0 +1,58 @@
+﻿import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function displayFact(e) {
+    const selectedAnimal = e.target.alt;
+
+    const animalInfo = animals[selectedAnimal];
+
+    const myIndex = Math.floor(Math.random() * animalInfo.facts.length);
+
+    const funFact = animalInfo.facts[myIndex];
+
+    document.getElementById('fact').innerHTML = funFact;
+
+}
+
+
+const title = '';
+
+const background = <img className='background'
+    src={'C:\Users\steve\repos\ReactAnimals\Pages\Shared\ocean.jpg'}
+    alt='ocean' />;
+const images = [];
+
+for (const animal in animals) {
+    images.push(<img
+
+        key={animal}
+
+        className='animal'
+        alt={animal}
+        src={animals[animal].image}
+
+        ariaLabel={animal}
+        role='button'
+        onClick={displayFact}
+
+    />)
+}
+
+const showBackground = true;
+const animalFacts = (
+    <div>
+        <h1>{title == '' ? 'Click an animal for a fun fact' : title}</h1>
+        {showBackground && background}
+
+        <div className='animals'>{images} </div>
+
+        <p id='fact'></p>
+    </div>
+);
+
+ReactDOM.render(
+
+    animalFacts, document.getElementById('root')
+);
